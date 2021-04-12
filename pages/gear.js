f6527e6eb46dc96f92ca825160f3f90045d139f6
David Torres <info@davidtorres.design>
2021-04-12 01:25:09 -0700

    Removed dev variables from Gear page

--- pages/gear.js
+++ pages/gear.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import getWowheadXML from "../utils/getWowheadXML.js";
 import getClassGear from "../utils/getClassGear";
 import filterGearByLevel from "../utils/filterGearByLevel";
 import separateGearByType from "../utils/separateGearByType";
@@ -17,13 +16,11 @@ const Gear = ({ appState }) => {
 	const { classGear, error, isPending } = getClassGear(playerClass);
 	const { gearData } = classGear ? filterGearByLevel(classGear, level) : [{}];
 	const separatedGearByType = gearData ? separateGearByType(gearData) : [{}, {}, {}];
-	const { xmldata, xmlerror } = getWowheadXML();
 
 	return (
 		<section className="content gear">
 			<div className="container">
 				<h1>Leveling Gear</h1>
-				{console.log(xmldata, xmlerror)}
 				<main>
 					{error ? (
 						<p>Error loading class gear. Let me know in a message!</p>
