e6b6ea74c31be440592086ee0cc042375ad72048
David Torres <info@davidtorres.design>
2021-10-23 22:53:03 -0700

    Remove SameSite Cookie Warning

--- pages/_document.js
+++ pages/_document.js
@@ -23,7 +23,7 @@ class MyDocument extends Document {
 								window.dataLayer = window.dataLayer || [];
 								function gtag(){dataLayer.push(arguments);}
 								gtag('js', new Date());
-								gtag('config', '${process.env.NEXT_PUBLIC_GA}');
+								gtag('config', '${process.env.NEXT_PUBLIC_GA}', {cookie_flags: 'SameSite=None;Secure'});
 							`,
 						}}
 					/>
